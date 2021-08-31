2bbdf31ee676f93b36e2d72519effbc5ac5c3626
Raven Townsend <rto45@uclive.ac.nz>
2021-09-01 10:46:07 +1200

    Add documentation to Person class

--- src/main/java/app/Person.java
+++ src/main/java/app/Person.java
@@ -1,30 +1,46 @@
 package app;
 
+
+/** Class that maps to the expected format of the input JSON file */
 public class Person {
 
+    // Attributes which map to expected JSON fields
     private String firstName;
     private String lastName;
     private String country;
     private int age;
 
+
+    /** Getter required for object mapping */
     public String getFirstName() {
         return firstName;
     }
+
+
+    /** Getter required for object mapping */
 
     public String getLastName() {
         return lastName;
     }
 
+
+    /** Getter required for object mapping */
     public String getCountry() {
         return country;
     }
 
+
+    /** Getter required for object mapping */
     public int getAge() {
         return age;
     }
 
+
     public String toString() {
-        return
-                "Name: " + firstName + lastName + ", Country: " + country + ", Age: " + age;
+        return firstName + " " + lastName + ", " + age + " (" + country + ")";
+    }
+
+    public String toStringWithoutCountry() {
+        return firstName + " " + lastName + ", " + age;
     }
 }
